6fb304b43622ac85c22046738b89ef7fb1137a92
RakibRahman <md.rr.talukder@gmail.com>
2025-01-03 12:13:00 +0600

    for loop

---
+++ for-loop.kt
@@ -0,0 +1,9 @@
+val cakes = listOf("carrot", "cheese", "chocolate");
+fun main() {
+    for (num in 1..10) {
+        println("$num ")
+    }
+    for (cake in cakes) {
+        println(cake)
+    }
+}
