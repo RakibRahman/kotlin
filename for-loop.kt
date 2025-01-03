e03b3ec872835368514eca59c442f82b4963a257
RakibRahman <md.rr.talukder@gmail.com>
2025-01-03 18:11:18 +0600

    for loop practice

--- for-loop.kt
+++ for-loop.kt
@@ -1,4 +1,36 @@
 val cakes = listOf("carrot", "cheese", "chocolate");
+val words = listOf("dinosaur", "limousine", "magazine", "language")
+
+fun fizzBuzz() {
+    for (number in 1..100) {
+        if (number % 3 == 0) {
+            println("fizz")
+        }
+        if (number % 5 == 0) {
+            println("buzz")
+        }
+    }
+}
+
+fun fizzBuzz2() {
+    for (number in 1..100) {
+        when {
+            (number % 15 == 0) -> println("FizzBuzz")
+            (number % 3 == 0) -> println("fizz")
+            (number % 5 == 0) -> println("Buzz")
+            else -> println("$number")
+        }
+    }
+}
+
+fun workChecker(){
+    for (word in words) {
+        if(word.startsWith("l")){
+            println(word)
+        }
+    }
+}
+
 fun main() {
     for (num in 1..10) {
         println("$num ")
@@ -6,4 +38,5 @@ fun main() {
     for (cake in cakes) {
         println(cake)
     }
+    workChecker()
 }
