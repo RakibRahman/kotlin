4a33595805fa7839170532863ecb6cd2c8b67102
RakibRahman <md.rr.talukder@gmail.com>
2025-01-03 10:06:18 +0600

    set example in kotlin

--- set.kt
+++ set.kt
@@ -1,6 +1,13 @@
 val readOnlyNames = setOf("John", "Peter", "Jane");
+val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
 
 val gamesSet = mutableSetOf("Witcher 3", "Uncharted", "Far Cry", "WWE","Far Cry");
+
+fun isSupported(){
+        val requested = "smtp"
+        val isSupported = requested.uppercase() in SUPPORTED
+        println("Support for $requested: $isSupported")
+}
 
 
 fun main(){
@@ -9,4 +16,5 @@ fun main(){
     gamesSet.add("Naruto Ninja War")
     gamesSet.remove("Far Cry")
     println(gamesSet)
+    println(isSupported())
 }
