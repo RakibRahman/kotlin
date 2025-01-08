66806270cc06b79e151bee67b9b4831df8fbb40d
RakibRahman <md.rr.talukder@gmail.com>
2025-01-08 09:20:35 +0600

    replaceVowels - codewars-kata

---
+++ exercises/replaceVowels.kt
@@ -0,0 +1,39 @@
+fun replaceVowels(s: String): String {
+    val vowels = "aeiouAEIOU".split("").filter { it.isNotEmpty() }
+    var output = ""
+    for (char in s) {
+        if (vowels.contains(char.toString())) {
+            output += "!"
+        } else {
+            output += char
+        }
+    }
+    return output
+}
+
+//with regex
+fun replaceVowelsV2(s: String): String {
+    return s.replace(Regex("[aeiouAEIOU]"), "!");
+}
+
+//forEach
+fun replaceVowelsV3(s: String): String {
+    var output = "";
+    s.forEach { char ->
+        if (char.lowercase() in "aeiou") {
+            output += "!"
+        } else {
+            output += char
+        }
+    }
+    return output
+}
+
+
+fun main() {
+    println(replaceVowels("apple"))
+    println(replaceVowels("aeiou"))
+    println(replaceVowelsV2("umbrella"))
+    println(replaceVowelsV3("umbrella"))
+    println(replaceVowelsV3("Elephant"))
+}
