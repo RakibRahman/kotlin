bc1b5b33e4173ef0e1f296cdef1dd03143e0ad26
RakibRahman <md.rr.talukder@gmail.com>
2025-01-10 22:19:05 +0600

    Taking Functions as Parameters

--- function.kt
+++ function.kt
@@ -9,9 +9,18 @@ fun printMessageWithPrefix(message: String, prefix: String = "Info") {
     println("[$prefix] $message")
 }
 
+//Taking Functions as Parameters
+
+fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
+    return operation(x, y);
+}
+
+fun sum(x: Int, y: Int): Int = x + y;
+
 fun main() {
     println(circleArea(2))
-    printMessageWithPrefix("code","mode")
+    printMessageWithPrefix("code", "mode")
     printMessageWithPrefix(message = "Info", prefix = "Info")  // Uses named arguments with swapped parameter order
+    println(calculate(2, 2, ::sum))
 }
 
